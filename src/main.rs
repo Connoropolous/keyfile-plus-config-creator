69dbbb4b3333b1c9b0a3284849da3c7c82e41233
Connor Turland <connorturland@gmail.com>
2019-05-23 19:14:26 -0400

    bump to develop latest

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,6 @@ use holochain_conductor_api::{
 };
 use holochain_dpki::SEED_SIZE;
 use std::{
-    env,
     fs::File,
     io::prelude::*,
     path::PathBuf,
@@ -38,7 +37,7 @@ keystore_file = "./keystore.key"
 
 [[dnas]]
 id = "chat_dna"
-file = "dna/holo-chat.dna.json"
+file = "dna/holochain-basic-chat.dna.json"
 
 [[instances]]
 id = "holo-chat"
@@ -55,7 +54,7 @@ path = "storage"
 id = "websocket_interface"
 [interfaces.driver]
 type = "websocket"
-port = 3400
+port = 8080
 [[interfaces.instances]]
 id = "holo-chat"
 
@@ -70,19 +69,19 @@ port = 3000
 dna_interface = "websocket_interface"
 
 [network]
-n3h_persistence_path = "/tmp"
+n3h_persistence_path = "./n3hfolder"
 n3h_log_level = "i"
+n3h_mode = "REAL"
+bootstrap_nodes=[]
 "#;
 
 pub fn main() {
-    let args: Vec<String> = env::args().collect();
     println!("Generating key file, please wait...");
-    let bootstrap_node = &args[1];
     let maybe_address = keygen(PathBuf::from("./keystore.key".to_string()), holochain_common::DEFAULT_PASSPHRASE.to_string());
     match maybe_address {
         Ok(address) => {
             let mut file = File::create(PathBuf::from("conductor-config.toml".to_string())).unwrap();
-            let contents = format!("{}public_address = \"{}\"{}bootstrap_nodes=[\"{}\"]", FIRST_HALF, address, SECOND_HALF, bootstrap_node);
+            let contents = format!("{}public_address = \"{}\"{}", FIRST_HALF, address, SECOND_HALF);
             let _ = file.write_all(contents.as_bytes());
             println!("Successfully wrote keystore.key and conductor-config.toml file");
         },
